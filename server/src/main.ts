1faa9608f699f3402c9454977b6b50c99813c757
locionic <noone000a@gmail.com>
2025-02-18 11:17:52 +0000

    Vercel deploy

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,16 +1,36 @@
-import express from 'express';
+// import express from 'express';
+// import cors from 'cors';
+// import { createServer } from 'http';
+// import routes from './app/routes';
+
+// const app = express();
+// const httpServer = createServer(app);
+
+// app.use(cors());
+// app.use(express.json());
+// app.use('/api', routes);
+
+// const PORT = process.env.PORT || 4000;
+// httpServer.listen(PORT, () =>
+//   console.log(`🚀 Server running at http://localhost:${PORT}`)
+// );
+
+
+import express, { Application } from 'express';
 import cors from 'cors';
-import { createServer } from 'http';
 import routes from './app/routes';
 
-const app = express();
-const httpServer = createServer(app);
+const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
 
-const PORT = process.env.PORT || 4000;
-httpServer.listen(PORT, () =>
-  console.log(`🚀 Server running at http://localhost:${PORT}`)
-);
+if (require.main === module) {
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () =>
+    console.log(`🚀 Server running at http://localhost:${PORT}`)
+  );
+}
+
+export default app;
